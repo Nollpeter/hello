adea694043b3bee259af4187e9e78dc2d566e954
Noll Péter <noll.peter@hotmail.com>
2016-05-25 20:14:29 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        f(10,11);
+    }
+    public static void f(int x, int y)
+    {
+        System.out.println(x+y);
+    }
+
+}
---
+++ src/MyClass.java
@@ -0,0 +1,14 @@
+/**
+ * Created by nollp on 2016. 05. 20..
+ */
+public class MyClass {
+    public int X;
+    public int Y;
+
+    public int getX() {
+        return X;
+    }
+    public int getY(){
+        return Y;
+    }
+}
